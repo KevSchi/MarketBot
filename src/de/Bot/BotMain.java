4792010d45ff93c6bb624111aa80e4bcfe2fd1af
fmueller0 <flo-steffen@freenet.de>
2022-06-26 13:35:29 +0200

    final

--- src/de/Bot/BotMain.java
+++ src/de/Bot/BotMain.java
@@ -9,6 +9,9 @@ import java.net.URL;
 import java.util.Base64;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 import java.util.*;
 
 import javax.json.Json;
@@ -27,10 +30,11 @@ import java.util.PriorityQueue;
 
 public class BotMain {
 
-	public static final String BASE_URL = "http://192.168.178.47:8080";
+	public static final String BASE_URL = "https://hackathon-game.relaxdays.cloud";
 	static final String GET_URL_SUPPLIER = BASE_URL + "/supplier";
 	private static final String GET_URL_Player_Self = BASE_URL + "/player/self";
-	public static final int PLAYER_ID = 0;
+	public static final int PLAYER_ID = 1;
+	static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 	// private static final String GET_URL_Article =
 	// "https://hackathon-game.relaxdays.cloud/article";
@@ -42,8 +46,8 @@ public class BotMain {
 	public static List<Listing> listings;
 	public static List<Artikel> articleFromSupplier;
 
-	static String authRelexdays = PLAYER_ID + ":" + "+wUshSS8PNniFaDf7bqEul1Vk9ED/fJt";
-	static String auth = PLAYER_ID + ":" + "jPjGYW7ErWG0knJB5a6Gxih+tHi2E5hT";
+	static String auth = PLAYER_ID + ":" + "+wUshSS8PNniFaDf7bqEul1Vk9ED/fJt";
+	// static String auth = PLAYER_ID + ":" + "jPjGYW7ErWG0knJB5a6Gxih+tHi2E5hT";
 
 	// static String encodedAuth =
 	// Base64.getEncoder().encodeToString(authRelaxdays.getBytes());
@@ -52,17 +56,21 @@ public class BotMain {
 	public static String SelfInfo = "0";
 	public static double MyCash = 0;
 	public static int runCount = 1;
+	public static int refreshTime = 1;
+	public static int timeGone = 0;
 	public static String Article;
 	public static String Supplier;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-
+		logger.setLevel(Level.ALL);
 		DataSource ds = DataSource.getInstance();
 		ds.prefillData();
 
 		articleFromSupplier = ds.getAlleArtikel();
 
 		while (true) {
+			timeGone = refreshTime * runCount;
+
 			listings = Listing.getListings();
 			Article = getArticle();
 			Supplier = Methods.getSupplier();
@@ -71,39 +79,33 @@ public class BotMain {
 			JsonArray supplierArray = jsonReader.readArray();
 			jsonReader.close();
 
-			// Wandle Json Strin der Artikel um in Json Array/Objekt
-			JsonReader jsonReader_art = Json.createReader(new StringReader(Article));
-			JsonArray articleArray = jsonReader_art.readArray();
-			jsonReader.close();
-
-			// System.out.println("BotMain - articleArray.size(): " + articleArray.size());
-
-			for (int i = 0; i < articleArray.size(); i++) {
-				Listing.getBestPrice(i);
-			}
+			// * Passt die Preise für diesesn Durchlauf an
+			Listing.updateBestPrices();
 
 			// getMostTags(10, artarr);
-
 			Methods.updatePrices(supplierArray);
 
 			// Einlesen self Infostring und convert 2 Json um Cash auszulesen
-
-			Methods.chooseWhatToBuy();
-
+			// ? Ausgabe in den Elsezweig
+			if (timeGone <= 5076000) { // 23,5*60*60 = Sekundenanzahl
+				Methods.chooseWhatToBuy();
+			} else {
+				logger.warning("Letzte halbe Stunde hat begonnen");
+			}
 			SelfInfo = sendGETplayerSelf();
 			JsonReader jsonReader_cash = Json.createReader(new StringReader(SelfInfo));
 			JsonArray Self_json = jsonReader_cash.readArray();
 			jsonReader.close();
 			MyCash = Self_json.getJsonObject(0).getJsonNumber("money").doubleValue();
 
-			System.out.println("Der Bot lief " + runCount++ + " Mal durch");
-			System.out.println("Dies ist mein Geld:  " + MyCash);
+			logger.info("Der Bot lief " + runCount++ + " - Dies ist mein Geld: " + MyCash);
 
 			// [{"id":1,"money":0.8045645,"stock":[{"article_id":0,"stock":1},{"article_id":12,"stock":7},{"article_id":33,"stock":1},{"article_id":34,"stock":132},{"article_id":49,"stock":80},{"article_id":67,"stock":109},{"article_id":84,"stock":198},{"article_id":96,"stock":254}]}]
 
 			// https://hackathon-game.relaxdays.cloud/player/self
 
 			JsonArray itemsInStock = Self_json.getJsonObject(0).getJsonArray("stock");
+			Methods.updateOfferPrices();
 
 			for (int index = 0; index < itemsInStock.size(); index++) {
 				int artid = itemsInStock.getJsonObject(index).getJsonNumber("article_id").intValue();
@@ -117,17 +119,18 @@ public class BotMain {
 						median = gegenstand.getMedian();
 					}
 				}
-				if (Listing.getBestPrice(artid) == 1) {
-					Listing.offer(artid, quantity, Listing.getBestPrice((int) median));
+				// ? BestPrice ist auf min 12 festgelegt
+				if (Listing.best_prices.get(artid) == 1) {
+					Listing.offer(artid, quantity, Listing.best_prices.get((int) median));
 				} else {
-					Listing.offer(artid, quantity, Listing.getBestPrice(artid));
+					Listing.offer(artid, quantity, Listing.best_prices.get(artid));
 				}
 
 				// System.out.println(Listing.getBestPrice(artid) + " Werde ich hier auch
 				// arm?");
 			}
 
-			TimeUnit.SECONDS.sleep(30);
+			TimeUnit.SECONDS.sleep(refreshTime);
 
 		}
 	}
@@ -147,6 +150,7 @@ public class BotMain {
 			// }
 		}
 
+		// ? Macht das noch irgendwas
 		List<Entry<Integer, Integer>> greatest = findGreatest(map, n);
 		// System.out.println("Top " + n + " entries:");
 
@@ -207,7 +211,7 @@ public class BotMain {
 			return response.toString();
 
 		} else {
-			System.out.println("GET request not worked");
+			logger.warning("GET request not worked");
 		}
 		return "error";
 
@@ -236,7 +240,7 @@ public class BotMain {
 			// print result
 			return response.toString();
 		} else {
-			System.out.println("GET request not worked");
+			logger.warning("GET request not worked");
 		}
 		return null;
 
